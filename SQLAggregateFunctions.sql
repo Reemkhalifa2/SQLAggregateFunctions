7173130033ab9ce258e45307a4595af6f856381a
Reem AL Adawi <aladawi12reem6@gmail.com>
2026-05-21 10:27:49 +0400

    SQL Aggregate Functions

---
+++ SQLAggregateFunctions.sql
@@ -0,0 +1,85 @@
+--Task 1: Basic Aggregate Functions
+SELECT SUM(Salary) AS Total_Salary,
+		SUM(Salary) / COUNT(Salary) AS Verage_Salary,
+		MAX(Salary) AS highest_salary,
+		MIN(Salary) AS lowest_salary,
+		COUNT(SSN)
+		FROM Employee;
+
+
+--Task 2: GROUP BY with Departments
+SELECT SUM(Salary) AS Total_Salary,
+		SUM(Salary) / COUNT(Salary) AS Verage_Salary,
+		COUNT(SSN)
+		FROM Employee
+		GROUP BY Dnum
+		ORDER BY Dnum;
+
+--Task 3: HAVING Clause Filtering
+SELECT Dnum ,SUM(Salary)AS Total_Salary FROM Employee
+GROUP BY Dnum
+HAVING SUM(Salary) > 10000;
+
+SELECT Dnum ,COUNT(SSN) AS Employees FROM Employee
+GROUP BY Dnum
+HAVING COUNT(SSN) > 5;
+
+SELECT Dnum ,SUM(Salary)/COUNT(SSN)AS Avrage_Salary FROM Employee
+GROUP BY Dnum
+HAVING SUM(Salary)/COUNT(SSN) > 5000;
+
+
+--Task 4: Orders Analysis
+
+SELECT 
+		SUM(Amount) as total_revenue , 
+		SUM(Amount)/Count(OrderID)  as AVerage,
+		COUNT(OrderID) as Total_Id
+		MAX(Amount) AS Max_amount,
+		MIN(Amount) AS Min_amount,
+		FROM Orders;
+
+SELECT 
+    CustomerID,
+    SUM(Amount) AS TotalSpent
+FROM Orders
+GROUP BY CustomerID;
+
+--Task 5: Customers Order Summary
+
+---1. Show number of orders per customer
+SELECT Customers.CustomerName ,COUNT(Orders.OrderID) AS TotalOrders
+FROM Customers
+LEFT JOIN Orders
+    ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID;
+
+
+---2. Show total amount spent per customer
+SELECT Customers.CustomerName ,SUM(Orders.Amount) AS Totalspent 
+FROM Customers
+LEFT JOIN Orders
+    ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID;
+
+
+---3. Show customers who spent more than 1000 total
+SELECT Customers.CustomerName ,SUM(Orders.Amount) AS Totalspent 
+FROM Customers
+LEFT JOIN Orders
+    ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID
+HAVING SUM(Orders.Amount) >1000;
+
+---4. Show customers with no orders
+
+SELECT 
+    Customers.CustomerID,
+    Customers.CustomerName
+FROM Customers
+LEFT JOIN Orders
+    ON Customers.CustomerID = Orders.CustomerID
+WHERE Orders.OrderID IS NULL;
+
+--Task 6: Sales Performance
+
